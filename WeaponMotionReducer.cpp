d4a1bb0c3acfed48b4d4c8e8e8b971bf0280a57d
piao <piao.polar@gmail.com>
2012-08-06 19:32:30 +0800

    1. 利用通配规则进一步优化； 2. 新增校验。

--- WeaponMotionReducer.cpp
+++ WeaponMotionReducer.cpp
@@ -8,6 +8,10 @@ const int _MAX_STRING = 1024;
 
 const char *pszIniReduced = "WeaponMotionReduced.ini";
 
+const __int64 WEAPON_MOTION_LOOK_MUTI = 10000;
+const __int64 WEAPON_MOTION_WEAPON_MUTI = 10000000;
+const __int64 WEAPON_MOTION_MOUNT_MUTI = 100000000000000;
+
 // ============================================================================
 // ==============================================================================
 
@@ -115,14 +119,17 @@ int main()
 		return 0;
 	}
 
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	std::map<__int64, std::string> mapReduceInfo;
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 	std::vector<NODE>::const_iterator it(vecOrder.begin());
 	for (; it != vecOrder.end(); ++it) {
 
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		__int64 i64Index = it->i64Index;
-		const __int64 WEAPON_MOTION_MOUNT_MUTI = 100000000000000;
 		__int64 i64Mount = i64Index / WEAPON_MOTION_MOUNT_MUTI;
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 		if (i64Mount > 1) {
 			i64Index -= (i64Mount - 1) * WEAPON_MOTION_MOUNT_MUTI;
@@ -130,16 +137,80 @@ int main()
 				ToolMsg("Error Index %I64d Res %s  !=  Reduce Index %I64d Res %s", it->i64Index, it->strRes.c_str(),
 						i64Index, mapOrgInfo[i64Index].c_str());
 				fprintf(pFileOut, "%s=%s\n", it->strIndex.c_str(), it->strRes.c_str());
+				mapReduceInfo[it->i64Index] = it->strRes;
 				++nNewCount;
 			}
 
 			continue;
 		}
 
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		__int64 i64Motion = i64Index % WEAPON_MOTION_LOOK_MUTI;
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		if (i64Motion != 9999) {
+
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			__int64 i64Motion9999 = i64Index / WEAPON_MOTION_LOOK_MUTI * WEAPON_MOTION_LOOK_MUTI + 9999;
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			if (mapOrgInfo[i64Motion9999] == it->strRes) {
+				continue;
+			}
+		}
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		__int64 i64Look = i64Index % WEAPON_MOTION_WEAPON_MUTI / WEAPON_MOTION_LOOK_MUTI;
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		if (i64Look != 999) {
+
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			__int64 i64Look999 = i64Index + (999 - i64Look) * WEAPON_MOTION_LOOK_MUTI;
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			if (mapOrgInfo[i64Look999] == it->strRes) {
+				continue;
+			}
+		} else {
+
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			__int64 i64WeaponQ = i64Index / WEAPON_MOTION_WEAPON_MUTI % 10;
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			if (i64WeaponQ) {
+
+				//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+				__int64 i64IndexQ0 = i64Index - i64WeaponQ * WEAPON_MOTION_WEAPON_MUTI;
+				//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+				if (mapOrgInfo[i64IndexQ0] == it->strRes) {
+					continue;
+				}
+			}
+		}
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		__int64 i64LookMotion = i64Index % WEAPON_MOTION_WEAPON_MUTI;
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		if (i64LookMotion != 9999999) {
+
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			__int64 i64LookMotion9999999 = i64Index / WEAPON_MOTION_WEAPON_MUTI * WEAPON_MOTION_WEAPON_MUTI + 9999999;
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			if (mapOrgInfo[i64LookMotion9999999] == it->strRes) {
+				continue;
+			}
+		}
+
 		if (i64Mount) {
 			i64Index -= WEAPON_MOTION_MOUNT_MUTI;
 			if (mapOrgInfo[i64Index] != it->strRes) {
 				fprintf(pFileOut, "%s=%s\n", it->strIndex.c_str(), it->strRes.c_str());
+
+				mapReduceInfo[it->i64Index] = it->strRes;
 				++nNewCount;
 			}
 
@@ -147,12 +218,204 @@ int main()
 		}
 
 		fprintf(pFileOut, "%s=%s\n", it->strIndex.c_str(), it->strRes.c_str());
+		mapReduceInfo[it->i64Index] = it->strRes;
 
 		++nNewCount;
 	}
 
 	fclose(pFileOut);
 	ToolMsg("Done. %d Index Save to %s", nNewCount, pszIniReduced);
+
+	ToolMsg("Checking...");
+
+	for (it = vecOrder.begin(); it != vecOrder.end(); ++it) {
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		__int64 i64Index = it->i64Index;
+		__int64 i64Mount = i64Index / WEAPON_MOTION_MOUNT_MUTI;
+		__int64 i64ActType = i64Index % WEAPON_MOTION_LOOK_MUTI;
+		__int64 id = i64Index % WEAPON_MOTION_MOUNT_MUTI - i64ActType;
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		if (i64Mount >= 1) {
+			id += WEAPON_MOTION_MOUNT_MUTI;
+		}
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		__int64 idMotion = id + i64ActType;
+		std::string strRes = mapReduceInfo[idMotion];
+		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id + 9999;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id -
+			((id / WEAPON_MOTION_LOOK_MUTI) % 1000) *
+			WEAPON_MOTION_LOOK_MUTI +
+			999 *
+			WEAPON_MOTION_LOOK_MUTI +
+			i64ActType;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = (id / WEAPON_MOTION_WEAPON_MUTI) * WEAPON_MOTION_WEAPON_MUTI + 9999999;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id -
+			((id / WEAPON_MOTION_LOOK_MUTI) % WEAPON_MOTION_LOOK_MUTI) *
+			WEAPON_MOTION_LOOK_MUTI +
+			999 *
+			WEAPON_MOTION_LOOK_MUTI +
+			i64ActType;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		if (!i64Mount) {
+			idMotion = 0;
+			strRes = "";
+			if (!it->strRes.empty()) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		id -= WEAPON_MOTION_MOUNT_MUTI;
+
+		idMotion = id + i64ActType;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id + 9999;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id -
+			((id / WEAPON_MOTION_LOOK_MUTI) % 1000) *
+			WEAPON_MOTION_LOOK_MUTI +
+			999 *
+			WEAPON_MOTION_LOOK_MUTI +
+			i64ActType;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = (id / WEAPON_MOTION_WEAPON_MUTI) * WEAPON_MOTION_WEAPON_MUTI + 9999999;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		idMotion = id -
+			((id / WEAPON_MOTION_LOOK_MUTI) % WEAPON_MOTION_LOOK_MUTI) *
+			WEAPON_MOTION_LOOK_MUTI +
+			999 *
+			WEAPON_MOTION_LOOK_MUTI +
+			i64ActType;
+		strRes = mapReduceInfo[idMotion];
+		if (!strRes.empty()) {
+			if (strRes != it->strRes) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		if (!i64Mount) {
+			idMotion = 0;
+			strRes = "";
+			if (!it->strRes.empty()) {
+				ToolMsg("Error Index: %I64d  OrgRes: %s  Reduced Index: %I64d  ReduceRes: %s", it->i64Index,
+						it->strRes.c_str(), idMotion, strRes.c_str());
+				break;
+			} else {
+				continue;
+			}
+		}
+	}
+
+	if (it == vecOrder.end()) {
+		ToolMsg("Checking Suc");
+	} else {
+		ToolMsg("Checking Failed");
+	}
 
 	while (getchar());
 
